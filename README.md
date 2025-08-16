71d12fc3bf75a6ce0ff870b107697f4667b10641
joaoMiraya <joaomiraya01@gmail.com>
2025-08-16 11:28:51 -0300

    feat: pascal's pyramid

---
+++ README.md
@@ -0,0 +1,18 @@
+Pt - BR
+## Algoritmos
+
+Este projeto foi desenvolvido com o objetivo de praticar e aprimorar habilidades em algoritmos, utilizando as linguagens Python, C++ e C#.
+
+O repositório contém implementações de diversos algoritmos clássicos e exercícios, servindo como base de estudo e experimentação para lógica de programação, estrutura de dados e boas práticas de desenvolvimento.
+
+
+---- 
+En - US
+
+## Algorithms
+
+This project was created to practice and improve algorithm skills using Python, C++, and C#.
+
+The repository includes implementations of various classic algorithms and exercises, serving as a study resource and a foundation for learning programming logic, data structures, and best coding practices.
+
+Quer que eu também adicione uma seção com exemplos do que contém no projeto (por exemplo, busca binária, ordenação, Fibonacci, etc.) e instruções para execução? Isso deixaria o README bem mais completo e profissional.
---
+++ csharp/Algorithims/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Numerics;
+
+// This is a Fibonacci sequence algorithm that returns the index of the Fibonacci number
+// that matches the input number. If the number is not a Fibonacci number, it returns -1.
+int fibonacciSequence(BigInteger num)
+{
+    if (num < 0) return -1;
+
+    BigInteger a = 0;
+    BigInteger b = 1;
+    int index = 1;
+
+    while (a < num)
+    {
+        BigInteger temp = a;
+        a = b;
+        b = temp + b;
+        index++;
+    }
+
+    return a == num ? index - 1 : -1;
+}
+
+Console.WriteLine("Fibonacci Sequence Index:");
+Console.WriteLine(fibonacciSequence(21));
+
+static void PascalPyramid()
+{
+    int rows = 10;
+
+    int[][] pascal = new int[rows][];
+
+    for (int i = 0; i < rows; i++)
+    {
+        pascal[i] = new int[i + 1];
+        pascal[i][0] = 1;
+        pascal[i][i] = 1;
+
+        for (int j = 1; j < i; j++)
+        {
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write(pascal[i][j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Pascal's Pyramid:");
+PascalPyramid();
